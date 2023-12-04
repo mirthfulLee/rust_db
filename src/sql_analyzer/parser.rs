2ee5d7ecb5c0033384aae959b4847bc8c01f4f0a
lihuan <lihuan0530@gmail.com>
2023-12-04 20:21:23 +0800

    implement insert parser

--- src/sql_analyzer/parser.rs
+++ src/sql_analyzer/parser.rs
@@ -1,16 +1,18 @@
+use std::str::FromStr;
+
 use nom_locate::LocatedSpan;
 // Using tag_no_case from nom_supreme since its error is nicer
 // ParserExt is mostly for adding `.context` on calls to identifier to say what kind of identifier we want
 use nom::{
     branch::alt,
     bytes::complete::{escaped, tag, take_while, take_while1},
-    character::complete::{alphanumeric1, char, multispace0, multispace1, one_of},
+    character::complete::{alphanumeric1, char, i32, multispace0, multispace1, one_of},
     combinator::{cut, map, opt, value},
     error::{context, convert_error, ContextError, ErrorKind, ParseError, VerboseError},
     multi::{separated_list0, separated_list1},
     number::complete::double,
     sequence::{delimited, preceded, separated_pair, terminated, tuple},
-    AsChar, Err, IResult, InputIter, InputLength, InputTakeAtPosition, Parser,
+    AsChar, Err, IResult, InputIter, InputLength, InputTake, InputTakeAtPosition, Parser,
 };
 use nom_supreme::{tag::complete::tag_no_case, ParserExt};
 use serde::{Deserialize, Serialize};
@@ -28,6 +30,13 @@ pub(crate) fn identifier(i: Span) -> ParseResult<String> {
     })(i)
 }
 
+pub fn comma_sep<'a, O, F>(f: F) -> impl FnMut(Span<'a>) -> ParseResult<'a, Vec<O>>
+where
+    F: FnMut(Span<'a>) -> ParseResult<'a, O>,
+{
+    separated_list1(tuple((multispace0, char(','), multispace0)), f)
+}
+
 /// Implement the parse function to more easily convert a span into a sql
 /// command
 pub trait Parse<'a>: Sized {
@@ -43,7 +52,7 @@ pub trait Parse<'a>: Sized {
 // many other imports omitted
 /// A colum's type
 #[derive(Debug, Clone, Eq, Hash, PartialEq, Serialize, Deserialize)]
-pub enum SqlTypeInfo {
+pub enum SqlType {
     // these are basic for now. Will add more + size max later on
     // TODO: add more types
     String,
@@ -51,7 +60,7 @@ pub enum SqlTypeInfo {
 }
 
 // parses "string | int"
-impl<'a> Parse<'a> for SqlTypeInfo {
+impl<'a> Parse<'a> for SqlType {
     fn parse(input: Span<'a>) -> ParseResult<'a, Self> {
         // context will help give better error messages later on
         context(
@@ -69,7 +78,7 @@ impl<'a> Parse<'a> for SqlTypeInfo {
 #[derive(Debug, Clone, Eq, Hash, PartialEq, Serialize, Deserialize)]
 pub struct Column {
     pub name: String,
-    pub type_info: SqlTypeInfo,
+    pub type_info: SqlType,
 }
 
 /// parses "<colName> <colType>"
@@ -81,7 +90,7 @@ impl<'a> Parse<'a> for Column {
                 separated_pair(
                     identifier.context("Column Name"),
                     multispace1,
-                    SqlTypeInfo::parse,
+                    SqlType::parse,
                 ),
                 |(name, type_info)| Self { name, type_info },
             ),
@@ -101,11 +110,7 @@ fn column_definitions(input: Span<'_>) -> ParseResult<'_, Vec<Column>> {
     context(
         "Column Definitions",
         map(
-            tuple((
-                char('('),
-                separated_list1(tuple((multispace0, char(','), multispace0)), Column::parse),
-                char(')'),
-            )),
+            tuple((char('('), comma_sep(Column::parse), char(')'))),
             |(_, cols, _)| cols,
         ),
     )(input)
@@ -136,26 +141,101 @@ impl<'a> Parse<'a> for CreateStatement {
     }
 }
 
+#[derive(Clone, Debug, Eq, Hash, PartialEq, Serialize, Deserialize)]
+pub enum SqlValue {
+    String(String),
+    Int(i32),
+}
+
+impl<'a> Parse<'a> for SqlValue {
+    fn parse(input: Span<'a>) -> ParseResult<'a, Self> {
+        alt((
+            map(alphanumeric1, |s: Span<'a>| {
+                Self::Int(s.to_string().parse::<i32>().unwrap())
+            }),
+            map(alphanumeric1, |s: Span<'a>| Self::String(s.to_string())),
+        ))(input)
+    }
+}
+
+#[derive(Clone, Debug, Default, Eq, Hash, PartialEq, Serialize, Deserialize)]
+pub struct RowValue {
+    pub values: Vec<SqlValue>,
+}
+
+impl<'a> Parse<'a> for RowValue {
+    fn parse(input: Span<'a>) -> ParseResult<'a, Self> {
+        // context will help give better error messages later on
+        map(
+            context(
+                "Value of Insert Row",
+                // alt will try each passed parser and return what ever succeeds
+                delimited(
+                    tuple((multispace0, char('('), multispace0)),
+                    comma_sep(SqlValue::parse),
+                    tuple((multispace0, char(')'), multispace0)),
+                ),
+            ),
+            |values| Self { values },
+        )(input)
+    }
+}
+
+/// The table and its columns to create
+#[derive(Clone, Debug, Default, Eq, Hash, PartialEq, Serialize, Deserialize)]
+pub struct InsertStatement {
+    pub table: String,
+    pub columns: Option<Vec<String>>,
+    pub values: RowValue,
+}
+
+impl<'a> Parse<'a> for InsertStatement {
+    fn parse(input: Span<'a>) -> ParseResult<'a, Self> {
+        map(
+            tuple((
+                delimited(
+                    // table name
+                    tuple((
+                        multispace1,
+                        tag_no_case("insert"),
+                        multispace1,
+                        tag_no_case("into"),
+                    )),
+                    identifier.context("Table Name"),
+                    tuple((multispace1, tag_no_case("values"))),
+                ),
+                opt(comma_sep(identifier)),
+                RowValue::parse,
+            ))
+            .context("Create Table"),
+            |(table, columns, values)| Self {
+                table,
+                columns,
+                values,
+            },
+        )(input)
+    }
+}
 // I was a test hater earlier but may as well cover the basics...
 #[cfg(test)]
 mod tests {
     use super::*;
     #[test]
-    fn test_create() {
+    fn test_create_stmt() {
         let expected = CreateStatement {
             table: "foo".into(),
             columns: vec![
                 Column {
                     name: "col1".into(),
-                    type_info: SqlTypeInfo::Int,
+                    type_info: SqlType::Int,
                 },
                 Column {
                     name: "col2".into(),
-                    type_info: SqlTypeInfo::String,
+                    type_info: SqlType::String,
                 },
                 Column {
                     name: "col3".into(),
-                    type_info: SqlTypeInfo::String,
+                    type_info: SqlType::String,
                 },
             ],
         };
@@ -167,5 +247,26 @@ mod tests {
             .1,
             expected
         )
+    }
+
+    #[test]
+    fn test_insert_stmt() {
+        let expected = InsertStatement {
+            table: String::from("foo"),
+            columns: None,
+            values: RowValue {
+                values: vec![
+                    SqlValue::String(String::from("abc")),
+                    SqlValue::Int(123),
+                    SqlValue::String(String::from("def")),
+                ],
+            },
+        };
+        let parse_result =
+            InsertStatement::parse_from_raw("INSERT INTO foo values ('abc', 123, 'def')")
+                .unwrap()
+                .1;
+        print!("{:?}", parse_result);
+        assert_eq!(parse_result, expected)
     }
 }
